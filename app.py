a53c9fd535d2f4e4aa66f666111495a5332f4efe
TKaoneka <kaonekat@gmail.com>
2025-09-21 21:22:56 +0300

    Update website templates

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template, session, request, redirect, abort, make_respo
 from werkzeug.security import generate_password_hash, check_password_hash
 import config
 import forum
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -17,15 +18,56 @@ def search():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    pass
+    if request.method == "GET":
+        return render_template("create_account.html")
+    
+    elif request.method == "POST":
+
+        username = request.form["new_username"]
+        password_1 = request.form["new_password"]
+        password_2 = request.form["new_password2"]
+        if password_1 != password_2:
+            viesti = "Salasanat eivät ole samat. Ole hyvä ja kokeile uudestaan"
+            return render_template("create_account.html", caution=viesti)
+        
+        hash = generate_password_hash(password_1)
+        try:
+            user_id = forum.create_account(username, hash)
+        except sqlite3.IntegrityError:
+            viesti = "Tunnus on jo käytössä. Ole hyvä ja kokeile uudestaan"
+            return render_template("new_account.html", caution=viesti)
+        session["id"] = user_id
+        session["username"] = username
+        return render_template("account_created.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    pass
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    elif request.method == "POST":
+        username = request.form["login_username"]
+        password = request.form["login_password"]
+
+        try:
+            info = forum.get_account(username)
+        except IndexError:
+            viesti = "Väärä tunnus tai salasana. Kokeile uudestaan :)"
+            return render_template("login.html", caution=viesti)
+        
+        if check_password_hash(info[1], password):
+            session["id"] = info[0]
+            session["username"] = username
+            return redirect("/")
+        else:
+            viesti = "Väärä tunnus tai salasana. Kokeile uudestaan :)"
+            return render_template("login.html", caution=viesti)
 
 @app.route("/logout")
 def logout():
+    del session["id"]
     del session["username"]
+
     return redirect("/")
 
 @app.route("/profile/<int:user_id>")
@@ -33,7 +75,11 @@ def show_profile(user_id):
     pass
 
 @app.route("/new_product")
-def create():
+def create_product():
+    pass
+
+@app.route("/modify_product")
+def modify_product():
     pass
 
 @app.route("/product/<int:product_id>")
--- assist.sql
+++ assist.sql
@@ -2,7 +2,8 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
     password_hash TEXT,
-    descript TEXT
+    descript TEXT,
+    image BLOB
 );
 
 CREATE TABLE posts (
@@ -12,7 +13,8 @@ CREATE TABLE posts (
     sub_title TEXT,
     time_posted TEXT,
     descript TEXT,
-    service_or_product INTEGER
+    product_info TEXT,
+    image BLOB
 );
 
 CREATE TABLE reviews (
@@ -29,4 +31,13 @@ CREATE TABLE likes (
     id INTEGER PRIMARY KEY,
     liker INTEGER REFERENCES users(id),
     product_id INTEGER REFERENCES posts(id)
+);
+
+CREATE TABLE messages (
+    id INTEGER PRIMARY KEY,
+    string TEXT,
+    product_id INTEGER REFERENCES posts(id),
+    messanger INTEGER REFERENCES users(id),
+    messanged INTEGER REFERENCES users(id),
+    time_posted TEXT
 );
